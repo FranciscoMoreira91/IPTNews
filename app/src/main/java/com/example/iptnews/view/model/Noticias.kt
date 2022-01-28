5c0b6a6a6584a2879f935fb8ff84512a0eff1199
ESTBrunoFreitas <est.bruno.freitas@pt.softinsa.com>
2022-01-28 09:26:28 +0000

    Update

--- app/src/main/java/com/example/iptnews/view/model/Noticias.kt
+++ app/src/main/java/com/example/iptnews/view/model/Noticias.kt
@@ -2,9 +2,22 @@ package com.example.iptnews.view.model
 
 
 data class Noticias(
-    val titulo : String?,
-    val autor : String?,
-    val descr : String?,
-    val DataP : String?,
-    val url : String?
+    @SerializedName("title")
+    val title : String?,
+    @SerializedName("pubDate")
+    val pubDate : String?,
+    @SerializedName("link")
+    val link : String?,
+    @SerializedName("guid")
+    val guid : String?,
+    @SerializedName("author")
+    val author : String?
+    @SerializedName("description")
+    val description : String?
+    @SerializedName("content")
+    val content : String?
+    @SerializedName("enclosure")
+    val enclosure : String?
+    @SerializedName("categories")
+    val categories : String?
 )
