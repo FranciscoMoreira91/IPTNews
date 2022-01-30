7b429fe979bc3351eedb32c9a2ce1ed22f569927
cc <aluno22907@ipt.pt>
2022-01-30 00:45:08 +0000

    menu

--- app/src/main/java/com/example/iptnews/view/fragmentListNews.kt
+++ app/src/main/java/com/example/iptnews/view/fragmentListNews.kt
@@ -8,7 +8,8 @@ import android.view.ViewGroup
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.iptnews.R
+import com.example.iptnews.*
+import com.example.iptnews.databinding.FragmentListNewsBinding
 import com.example.iptnews.view.viewmodel.ListViewModel
 import kotlinx.android.synthetic.main.fragment_details_news.*
 import kotlinx.android.synthetic.main.fragment_list_news.*
@@ -24,8 +25,46 @@ class fragmentListNews : Fragment(){
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list_news, container, false)
+
+        val bind = FragmentListNewsBinding.inflate(layoutInflater)
+        val fragmenteconomic = EconomicFragment()
+
+        bind.btnToEconomic.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.fragmentContainerView, fragmenteconomic, EconomicFragment::class.java.simpleName )
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+        val fragmentpolitica = PoliticaFragment()
+
+        bind.btnToPolitica.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.fragmentContainerView, fragmentpolitica, PoliticaFragment::class.java.simpleName )
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+        val fragmentsport = SportsFragment()
+
+        bind.btnToSport.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.fragmentContainerView, fragmentsport, SportsFragment::class.java.simpleName )
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
+
+
+        val fragmentmundo = MundoFragment()
+        bind.btnToMundo.setOnClickListener {
+            fragmentManager?.beginTransaction()?.apply {
+                replace(R.id.fragmentContainerView, fragmentmundo, MundoFragment::class.java.simpleName)
+                    .addToBackStack(null)
+                    .commit()
+            }}
+
+        return bind.root
 
     }
 
